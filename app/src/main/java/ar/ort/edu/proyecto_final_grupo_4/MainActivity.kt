677a4ffe37c348c5fec4886e48d634efd124e22f
unknown <Asus@Angel.ANGEL>
2025-05-29 18:45:23 -0300

    Se agrega login con acceso biométrico

--- app/src/main/java/ar/ort/edu/proyecto_final_grupo_4/MainActivity.kt
+++ app/src/main/java/ar/ort/edu/proyecto_final_grupo_4/MainActivity.kt
@@ -1,65 +1,69 @@
 package ar.ort.edu.proyecto_final_grupo_4
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ar.ort.edu.proyecto_final_grupo_4.ui.screens.Screens
 import ar.ort.edu.proyecto_final_grupo_4.ui.screens.addMedication.AddMedicationScreen
+import ar.ort.edu.proyecto_final_grupo_4.ui.screens.biometricLogin.BiometricLoginScreen
 import ar.ort.edu.proyecto_final_grupo_4.ui.screens.homeDashboard.HomeScreen
 import ar.ort.edu.proyecto_final_grupo_4.ui.theme.ProyectoFinalGrupo4Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ProyectoFinalGrupo4Theme {
-
-                val navController = rememberNavController()
-
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    innerPadding ->
-
-                    NavHost(
-                        modifier= Modifier.padding(innerPadding),
-                        navController=navController,
-                        startDestination= Screens.Home.screen
-                    ){
-                        composable(Screens.Home.screen){ HomeScreen(navController)}
-                        composable(Screens.AddMedication.screen){ AddMedicationScreen()}
-                    }
-                }
+                MainNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun MainNavigation() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProyectoFinalGrupo4Theme {
-        Greeting("Android")
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        NavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+            startDestination = Screens.BiometricLogin.screen
+        ) {
+            composable(Screens.BiometricLogin.screen) {
+                BiometricLoginScreen(
+                    onAuthenticationSuccess = {
+                        navController.navigate(Screens.Home.screen) {
+                            popUpTo(Screens.BiometricLogin.screen) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    onAuthenticationError = { error ->
+                        // Manejar errores si es necesario
+                    }
+                )
+            }
+
+            composable(Screens.Home.screen) {
+                HomeScreen(navController)
+            }
+
+            composable(Screens.AddMedication.screen) {
+                AddMedicationScreen()
+            }
+        }
     }
 }
--- app/src/main/java/ar/ort/edu/proyecto_final_grupo_4/ui/screens/Screens.kt
+++ app/src/main/java/ar/ort/edu/proyecto_final_grupo_4/ui/screens/Screens.kt
@@ -1,6 +1,7 @@
 package ar.ort.edu.proyecto_final_grupo_4.ui.screens
 
 sealed class Screens(val screen: String) {
+    data object BiometricLogin : Screens("BiometricLogin")
     data object Home: Screens("Home")
     data object AddMedication: Screens("AddMedication")
     data object History: Screens("History")
